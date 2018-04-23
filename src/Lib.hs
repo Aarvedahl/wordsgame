1c7dce83d044b39c16a7ad3c78fcdc2bb27412ed
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-04-23 21:56:50 +0200

    Beginning to make the grid

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,13 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+someString :: String
+someString = "someString"
+
+grid = [  "_ _C _ _ _ _ _ _ _R_ _ _"
+           , " _ _SI_ _ _ _ _ _ _ _U_ _"
+           , " _ _HASKELL _ _ _ _B_"
+           , " _ _A_ _A_ _ _ _ _ S_ _Y"
+           , "  "
+]
