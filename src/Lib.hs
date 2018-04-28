4b127bfb18e78a7bd881f4f2498b421890b9ddee
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-04-28 15:28:17 +0200

    corrected errors

--- src/Lib.hs
+++ src/Lib.hs
@@ -19,16 +19,16 @@ outputGrid grid = putStrLn (formatGrid grid)
 formatGrid :: Grid -> String
 formatGrid = unlines
 
-findWord :: Grid -> String ->Bool
+findWord :: Grid -> String -> Maybe String
 findWord grid word=
   let lines = grid ++ ( map reverse grid)
-        found = or $ map (findWordInLine word) lines
+      found = or $ map (findWordInLine word) lines
   in if found then Just word else Nothing
 
---findWords :: Grid -> [String]  -> [String]
+---findWords :: Grid -> [String]  -> [String]
 findWords grid words =
   let foundWords =  map (findWord grid) words
-  in catMaybes foudnWords
+  in catMaybes foundWords
 
 
 findWordInLine :: String -> String -> Bool
