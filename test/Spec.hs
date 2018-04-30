912e2ac328929fff4779bd8ed01ae80a0b1b440a
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-04-30 23:36:00 +0200

    two unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,3 +9,6 @@ main = hspec $ do
     it "joins up a grid into a string" $ do
       formatGrid ["abc", "def", "ghi"] `shouldBe` "abc\ndef\nghi\n"
 
+  describe "findWord" $ do
+    it "Should find words that exists on the Grid" $ do
+      findWord grid "HASKELL" `shouldBe` Just "HASKELL"
