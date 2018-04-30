6a4058a0d2980c66fbb3da31723afbbadaef0211
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-04-30 23:28:16 +0200

    Successful unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,11 @@
 import Lib
 import Test.Hspec
-import Test.QuickCheck
-import Control.Exception (evaluate)
+--import Test.QuickCheck
+--import Control.Exception (evaluate)
 
 main :: IO ()
 main = hspec $ do
-describe " formatGrid" $ do
-   it "Should concatenate every line with a newline" $ do
-      (formatGrid ["abc", "def", "ghi"]) `shouldBe` "abc\ndef\nghi"
+  describe "formatGrid" $ do
+    it "joins up a grid into a string" $ do
+      formatGrid ["abc", "def", "ghi"] `shouldBe` "abc\ndef\nghi\n"
 
